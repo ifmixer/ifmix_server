3ceaddd5074acafd6394a228bed526a9979762a4
MyLab <mylab@qq.com>
2026-08-09 22:37:18 +0800

    wrk

---
+++ core-api/src/main/wrk/findTodoById/findById.lua
@@ -0,0 +1,18 @@
+wrk.method = "PUT"
+
+wrk.body = [[
+{
+  "id": "6a788b4994c98c57a0119a3f"
+}
+]]
+
+wrk.headers["accept"] = "application/json"
+wrk.headers["x-app-id"] = "6a7884d0fd585ec69ce5f1b7"
+wrk.headers["x-install-id"] = "48c651ed-84d7-4a54-a7be-6ec365bac52c"
+wrk.headers["x-lang"] = "zh-CN"
+wrk.headers["x-currency"] = "USD"
+wrk.headers["x-country"] = "CN"
+wrk.headers["x-native-version"] = "1.0.5"
+wrk.headers["x-js-version"] = "3.2.6"
+wrk.headers["x-client-platform"] = "ios"
+wrk.headers["Content-Type"] = "application/json"
---
+++ core-api/src/main/wrk/findTodoById/findById.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wrk -t4 -c100 -d30s --latency \
+  -s findById.lua \
+  http://localhost:3001/customer/core/query/todo/getById
